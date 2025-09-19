b9cc08a90698628e30ee5adc0904bb8f730e60db
thucnb <thucnb@newwave.com.vn>
2025-09-19 15:48:33 +0700

    apen api

--- src/main/java/com/techweek/n8n_demo/controller/MenuController.java
+++ src/main/java/com/techweek/n8n_demo/controller/MenuController.java
@@ -84,6 +84,7 @@ public class MenuController {
         System.out.println("gigi");
         System.out.println("gigi");
         System.out.println("gigi");
+        System.out.println("gigi");
         
         return "menu";
     }
